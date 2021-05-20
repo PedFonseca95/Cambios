b0a1de0162415b9f5dffd4289c411a17e994db26
PedFonseca95 <pedro.ricardo.fonseca1995@gmail.com>
2021-05-20 11:42:28 +0100

    ApiService and NetworkService

--- Cambios/Form1.cs
+++ Cambios/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using Cambios.Modelos;
+using Cambios.Servicos;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
@@ -15,9 +16,21 @@ namespace Cambios
 {
     public partial class Form1 : Form
     {
+        #region Atributos
+
+        private NetworkService networkService;
+
+        private ApiService apiService;
+
+        #endregion
+
+        public List<Rate> Rates { get; set; } = new List<Rate>();
+
         public Form1()
         {
             InitializeComponent();
+            networkService = new NetworkService();
+            apiService = new ApiService();
             LoadRates();
         }
 
@@ -25,35 +38,42 @@ namespace Cambios
         {
             //bool load; // Para saber se foi carregado ou não
 
-            progressBar1.Value = 0;
+            lbl_resultado.Text = "A atualizar taxas...";
 
-            var client = new HttpClient(); // Criar conexão via http
+            var connection = networkService.CheckConnection(); // Vai testar a conexão à internet
 
-            client.BaseAddress = new Uri("http://cambios.somee.com"); // Definir endereço base/principal da API
-
-            var response = await client.GetAsync("/api/rates"); // Definir o controlador da API (resto do link)
-            // async e await serve para que a aplicação continue a correr enquanto são carregadas as taxas - Tarefa asincrona
-
-            var result = await response.Content.ReadAsStringAsync(); // Carregar os resultados obtidos em formato string -> JSON
-
-            if (!response.IsSuccessStatusCode) // Caso haja algum problema/não seja obtida uma resposta por parte da API
+            if (!connection.IsSuccess) // Se a conexão não tiver sido feita com sucesso
             {
-                MessageBox.Show(response.ReasonPhrase); // Mostra uma mensagem com o motivo pela qual a resposta não foi feita com sucesso
-                return; // Termina o método
+                MessageBox.Show(connection.Message);
+                return; // Sai do método caso não haja conexão feita
+            }
+            else // Se tiver conexão
+            {
+                await LoadApiRates();
             }
 
-            // Caso tenha obtido uma resposta
-            var rates = JsonConvert.DeserializeObject<List<Rate>>(result); // Converte o resultado JSON obtido (result) para dentro de uma lista com dados do tipo Rate
 
-            cb_origem.DataSource = rates; // Apresentar os valores da lista criada rates na comboBox origem
+            cb_origem.DataSource = Rates; // Apresentar os valores da lista criada rates na comboBox origem
             cb_origem.DisplayMember = "Name"; // Mostrar a propriedade Name nos items da comboBox 
 
             cb_destino.BindingContext = new BindingContext(); // Permite que as comboBox deixem de estar ligadas/binding, permitindo escolher items diferentes
 
-            cb_destino.DataSource = rates;
+            cb_destino.DataSource = Rates;
             cb_destino.DisplayMember = "Name";
 
             progressBar1.Value = 100;
+            lbl_resultado.Text = "Taxas carregadas com sucesso!";
+        }
+
+        private async Task LoadApiRates()
+        {
+            progressBar1.Value = 0;
+
+            // Definir endereço base/principal e controlador da API
+            var response = await apiService.GetRates("http://cambios.somee.com", "/api/rates");
+            // async e await serve para que a aplicação continue a correr enquanto são carregadas as taxas - Tarefa asincrona  
+
+            Rates = (List<Rate>)response.Result;
         }
     }
 }
--- Cambios/Servicos/ApiService.cs
+++ Cambios/Servicos/ApiService.cs
@@ -1,13 +1,58 @@
-﻿using System;
+﻿using Cambios.Modelos;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace Cambios.Servicos
 {
-    class ApiService
+    public class ApiService
     {
         // Em vez de ter o código de ligação à API diretamente no form, utilizamos esta classe para o fazer, utilizando um serviço
+
+        public async Task<Response> GetRates(string urlBase, string controller) // Método com uma tarefa atribuida (Task) de obter uma resposta (response)
+        {
+            // Sempre que são feitas ligações a bases de dados, utilizar SEMPRE um try/catch
+
+            try
+            {
+                var client = new HttpClient(); // Criar conexão via http
+
+                client.BaseAddress = new Uri(urlBase); // Definir endereço base/principal da API
+
+                var response = await client.GetAsync(controller); // Definir o controlador da API (resto do link)
+                // async e await serve para que a aplicação continue a correr enquanto são carregadas as taxas - Tarefa asincrona
+
+                var result = await response.Content.ReadAsStringAsync(); // Carregar os resultados obtidos em formato string -> JSON
+
+                if (!response.IsSuccessStatusCode) // Caso haja algum problema/não seja obtida uma resposta por parte da API
+                {
+                    return new Response // Retorna uma nova resposta (falhou)
+                    {
+                        IsSuccess = false,
+                        Message = result
+                    };
+                }
+
+                // Caso tenha obtido uma resposta
+                var rates = JsonConvert.DeserializeObject<List<Rate>>(result); // Converte o resultado JSON obtido (result) para dentro de uma lista com dados do tipo Rate
+
+                return new Response // Retorna uma nova resposta (sucesso)
+                {
+                    IsSuccess = true,
+                    Result = rates
+                };
+
+            }
+            catch (Exception ex) // No caso de haver outro problema qualquer
+            {
+                return new Response
+                {
+                    IsSuccess = false,
+                    Message = ex.Message
+                };
+            }
+        }
     }
 }
